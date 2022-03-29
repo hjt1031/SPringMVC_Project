ba2c96ea8eef1aece668cf5726e88fdf180cbc6b
hjt1031 <hjt1031@naver.com>
2022-03-29 15:36:12 +0900

    상품수정

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -6,10 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -49,9 +46,62 @@ public class BasicItemController {
         return "basic/addForm";
     }
 
+//    @PostMapping("/add")
+    public String addItemV1(@RequestParam String itemName,
+                       @RequestParam int price,
+                       @RequestParam Integer quantity,
+                       Model model){
+        Item item = new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+
+        itemRepository.save(item);
+
+        model.addAttribute("item",item);
+        return "basic/item";
+    }
+
+
+//    @PostMapping("/add")
+    public String addItemV2(@ModelAttribute("item") Item item,
+                       Model model){
+
+        itemRepository.save(item);
+
+//        model.addAttribute("item",item); // 자동 추가, 생략 가능
+        return "basic/item";
+    }
+
+//    @PostMapping("/add")
+    //ModelAttribute 명명 을 안정해주면 클래스 명 첫글자를 소문자로 바꿔서 넣어준다 ex) Item => item
+    public String addItemV3(@ModelAttribute Item item){
+        itemRepository.save(item);
+        return "basic/item";
+    }
+
     @PostMapping("/add")
-    public String save(){
-        return "basic/addForm";
+    // ModelAttribute 생략 가능
+    public String addItemV4(Item item){
+        itemRepository.save(item);
+        return "basic/item";
+    }
+
+
+    //상품 수정 폼
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item",item);
+        return "basic/editForm";
+    }
+
+    //상품 수정 처리
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable long itemId, @ModelAttribute Item item) {
+        itemRepository.update(itemId, item);
+        //리다이렉트
+        return "redirect:/basic/items/{itemId}";
     }
 
     // 테스트용 데이터 추가.
@@ -60,4 +110,5 @@ public class BasicItemController {
         itemRepository.save(new Item("itemA",10000,10));
         itemRepository.save(new Item("itemB",20000,20));
     }
+
 }
